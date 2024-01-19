6203d0e6d4d0fa43da961c2707f23e57c07b9599
Sahil Shah <shahsahils100@gmail.com>
2024-01-19 18:55:23 +0530

    Added colored_labeled based on intensity of pixels; k =6

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import cv2
+from colors import colors
 
 description_file = pd.read_csv(os.path.join("ordered_images", "labels_ordered.csv"))
 
@@ -17,6 +18,15 @@ images = []
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
 K = 6
 
+color_labels = [
+    list(colors.BLACK),
+    list(colors.WHITE),
+    list(colors.GREEN),
+    list(colors.BLUE),
+    list(colors.YELLOW),
+    list(colors.RED),
+]
+
 i = 1
 for file in image_files:
     image = cv2.imread(os.path.join("ordered_images", file))
@@ -30,14 +40,18 @@ for file in image_files:
         pixel_vals, K, None, CRITERIA, 10, cv2.KMEANS_RANDOM_CENTERS
     )
 
-    centers = np.uint8(centers)
-    segmented_data = centers[labels.flatten()]
+    new_centers = np.zeros((K, 3), dtype=np.uint8)
 
-    print(labels.flatten())
-    # Continue from here, run the code once. The centers are the value of color the cluster appears. Try changing the value of centers and the clusters will appear colorful
-    break
+    for j in range(0, K):
+        idx = np.where(min(centers) == centers)[0][0]
+        new_centers[idx] = color_labels[j]
+        centers[idx][0] = 1e7
 
-    segmented_image = segmented_data.reshape((image.shape))
+    new_centers = np.uint8(new_centers)
+
+    segmented_data = new_centers[labels.flatten()]
+
+    segmented_image = segmented_data.reshape((image.shape[0], image.shape[1], 3))
 
     cv2.imwrite(os.path.join(output_path, file), segmented_image)
     print("Done " + str(i))
