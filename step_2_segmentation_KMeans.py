fc4bc9205e57fe3fc739350006750cf84ed3f0f5
Sahil Shah <shahsahils100@gmail.com>
2024-04-22 02:46:19 +0530

    Step-2 Completed

--- step_2_segmentation_KMeans.py
+++ step_2_segmentation_KMeans.py
@@ -12,21 +12,93 @@ local_centers = pd.read_csv(
     "output_images\KMeans_K_is_8_with_centres\output_centers.csv", index_col=0
 )
 
-center_array = np.array([])
+global_K = 8
+
+centers_array = np.array([])
+
+color_labels = [
+    list(colors.BLACK),
+    list(colors.WHITE),
+    list(colors.GREEN),
+    list(colors.CYAN),
+    list(colors.BLUE),
+    list(colors.YELLOW),
+    list(colors.ORANGE),
+    list(colors.RED),
+]
 
 
 for file in local_centers[dataset.Variables.Filename]:
 
-    center_i = local_centers.loc[
+    centers = local_centers.loc[
         local_centers[dataset.Variables.Filename] == file, dataset.Variables.Centers
     ]
 
-    center_i = center_i.tolist()
+    centers = centers.tolist()
 
-    list_value = literal_eval(center_i[0])
+    centers_list = literal_eval(centers[0])
 
-    center_array = np.append(center_array, np.array(list_value[0]))
-    center_array = np.uint8(center_array)
+    centers_array = np.append(centers_array, np.array(centers_list))
+    centers_array = np.uint8(centers_array)
 
 
-center_array = center_array.reshape((-1, 1))
+centers_array = centers_array.reshape((-1, 1))
+
+# Global KMeans Object
+global_KMeans = KMeans(n_clusters=global_K, random_state=1234, n_init="auto").fit(
+    centers_array
+)
+
+dataset.INPUT_PATH = "output_images\Denoised_Images\S1_100k_fastNLmeansdenoising"
+
+# Reading Description File
+description_file = pd.read_csv(
+    os.path.join(dataset.INPUT_PATH, "description_file.csv"), index_col=0
+)
+
+# Setting Output Directory
+dataset.output_dir("Step_2_KMeans_K_8")
+
+for file in description_file[dataset.Variables.Filename]:
+    # Loading the selected image
+    denoised_image = cv2.imread(os.path.join(dataset.INPUT_PATH, file))
+    denoised_image = cv2.cvtColor(denoised_image, cv2.COLOR_BGR2GRAY)
+
+    # Taking pixel_values of image as 1D array
+    pixel_vals = denoised_image.reshape((-1, 1))
+    pixel_vals = np.uint8(pixel_vals)
+
+    # Predicting labels
+    image_labels = global_KMeans.predict(pixel_vals)
+
+    centers_new = [
+        np.uint8(np.average(pixel_vals[image_labels == i])) for i in range(0, global_K)
+    ]
+
+    # Assigning color labels in ascending order
+    new_colors = [[0, 0, 0]] * global_K
+
+    for i in range(0, global_K):
+        idx = centers_new.index(min(centers_new))
+        new_colors[idx] = color_labels[i]
+        centers_new[idx] = 1e7
+
+    new_colors = np.array(new_colors)
+    new_colors = np.uint8(new_colors)
+
+    # Segmenting data with colors
+    segmented_data = new_colors[image_labels.flatten()]
+
+    # Reshaping segmented data into image dimensions
+    segmented_data = segmented_data.reshape(
+        (denoised_image.shape[0], denoised_image.shape[1], 3)
+    )
+
+    cv2.imwrite(os.path.join(dataset.OUTPUT_PATH, file), denoised_image)
+
+    cv2.imwrite(
+        os.path.join(dataset.OUTPUT_PATH, file[:-11] + "_global_labeled.tif"),
+        segmented_data,
+    )
+
+    print("Done " + file)
